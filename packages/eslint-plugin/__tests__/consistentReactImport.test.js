2b8205c25e323cdbf7f4c7e5e86f5b5280100a80
SimeonC <simeon@thehavens.co.nz>
2022-03-28 12:15:25 +0900

    fix(eslint-plugin): correctly handle jsx fragments
    Fixes #49

--- packages/eslint-plugin/__tests__/consistentReactImport.test.js
+++ packages/eslint-plugin/__tests__/consistentReactImport.test.js
@@ -1,9 +1,18 @@
-const { RuleTester } = require('eslint');
+const {
+  ESLintUtils: { RuleTester }
+} = require('@typescript-eslint/utils');
 
 const rule = require('../src/consistentReactImport');
 
 const ruleTester = new RuleTester({
-  parserOptions: { ecmaVersion: 2015, sourceType: 'module' }
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  }
 });
 
 const messageId = 'consistentReactImport';
@@ -48,6 +57,74 @@ const invalidTests = [
     function useHook() {
       const [val, setVal] = ReactCustom.useState(true);
       return ReactCustom.useMemo(() => val, []);
+    }`,
+    errors: [
+      {
+        messageId
+      }
+    ]
+  },
+  {
+    code: `
+    import { Fragment } from 'react';
+    function Component() {
+      return <Fragment>Something</Fragment>;
+    }`,
+    output: `
+    import * as React from 'react';
+    function Component() {
+      return <React.Fragment>Something</React.Fragment>;
+    }`,
+    errors: [
+      {
+        messageId
+      }
+    ]
+  },
+  {
+    code: `
+    import { Fragment as Fr } from 'react';
+    function Component() {
+      return <Fr>Something</Fr>;
+    }`,
+    output: `
+    import * as React from 'react';
+    function Component() {
+      return <React.Fragment>Something</React.Fragment>;
+    }`,
+    errors: [
+      {
+        messageId
+      }
+    ]
+  },
+  {
+    code: `
+    import { Fragment } from 'react';
+    function Component() {
+      return <Fragment key="one">Something</Fragment>;
+    }`,
+    output: `
+    import * as React from 'react';
+    function Component() {
+      return <React.Fragment key="one">Something</React.Fragment>;
+    }`,
+    errors: [
+      {
+        messageId
+      }
+    ]
+  },
+  {
+    code: `
+    import { Fragment } from 'react';
+    function Component() {
+      return <Fragment key="one"><div><>Inner <Fragment>Center</Fragment></> Outer</div></Fragment>;
+    }`,
+    output: `
+    import * as React from 'react';
+    function Component() {
+      return <React.Fragment key="one"><div><>Inner <React.Fragment>Center</React.Fragment></> Outer</div></React.Fragment>;
     }`,
     errors: [
       {
