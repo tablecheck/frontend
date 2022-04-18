4de60e78cae6105dda600c5749518b8fcf7aedf0
SimeonC <simeon@thehavens.co.nz>
2022-04-18 19:21:44 +0900

    fix(scripts): the output of app based lib exports should correctly be calculated
    This deals with an issue where the absolute imports in an app were not getting transformed to relative and the an excess of definition files for irrelevant content.

--- packages/scripts/scripts/rollup/buildPackage.js
+++ packages/scripts/scripts/rollup/buildPackage.js
@@ -4,21 +4,28 @@ const util = require('util');
 const chalk = require('chalk');
 const fs = require('fs-extra');
 const rollup = require('rollup');
-const typescript = require('rollup-plugin-typescript2');
+const glob = require('glob');
+const typescriptPlugin = require('@rollup/plugin-typescript');
+const ts = require('typescript');
 const { getBabelOutputPlugin } = require('@rollup/plugin-babel');
 const { nodeResolve } = require('@rollup/plugin-node-resolve');
 const commonjs = require('@rollup/plugin-commonjs');
 const json = require('@rollup/plugin-json');
-const externals = require('rollup-plugin-node-externals');
+const { externals } = require('rollup-plugin-node-externals');
 
 const paths = require('../../config/paths');
 const { getArgv } = require('../utils/argv');
-const { isLibTypeDefinitions } = require('../utils/configureTypescript');
+const {
+  isLibTypeDefinitions,
+  configureLibTypescript
+} = require('../utils/configureTypescript');
 const { logTaskEnd, logTaskStart } = require('../utils/taskLogFormatter');
 
 const { jsxPlugin } = require('./jsxPlugin');
 
 const argv = getArgv();
+
+const tsPrinter = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
 
 function getBundleInput(packageDirectory) {
   const pkgFile = fs.readJsonSync(path.join(packageDirectory, 'package.json'));
@@ -37,9 +44,9 @@ function getBundleInput(packageDirectory) {
     });
   }
   const tsEntry = path.join(packageDirectory, 'src/index.ts');
-  if (fs.existsSync(tsEntry)) return tsEntry;
+  if (fs.existsSync(tsEntry)) return [tsEntry];
   const tsxEntry = path.join(packageDirectory, 'src/index.tsx');
-  if (fs.existsSync(tsxEntry)) return tsxEntry;
+  if (fs.existsSync(tsxEntry)) return [tsxEntry];
   throw new Error(
     `Package entries/entry key was not set and could not find a valid "index.ts" or "index.tsx" in "${path.join(
       packageDirectory,
@@ -70,6 +77,158 @@ function getBabelPlugin(isEsm = false) {
   });
 }
 
+function loadRollupConfig(
+  packagePath,
+  rootPackageDirectory,
+  packageDirectory,
+  bundleEntries,
+  rollupWarnings
+) {
+  const rollupExternalsConfig = {
+    packagePath: [packagePath, path.join(rootPackageDirectory, 'package.json')],
+    deps: true
+  };
+  const rollupTypescriptConfig = {
+    tsconfig: path.join(packageDirectory, 'tsconfig.json'),
+    compilerOptions: {
+      isolatedModules: false
+    }
+  };
+  const rollupConfig = {
+    input: bundleEntries,
+    preserveModules: true,
+    treeshake: false,
+    onwarn: (warning) => {
+      // https://github.com/rollup/rollup/blob/0fa9758cb7b1976537ae0875d085669e3a21e918/src/utils/error.ts#L324
+      if (warning.code === 'UNRESOLVED_IMPORT') {
+        rollupWarnings.push(
+          `Failed to resolve the module ${warning.source} imported by ${warning.importer}` +
+            `\nIs the module installed? Note:` +
+            `\n ↳ to inline a module into your bundle, install it to "devDependencies".` +
+            `\n ↳ to depend on a module via import/require, install it to "dependencies".`
+        );
+        return;
+      }
+
+      if (warning.code === 'UNUSED_EXTERNAL_IMPORT') {
+        console.warn(chalk.yellow('\n', warning.message));
+      }
+      rollupWarnings.push(warning);
+    },
+    plugins: [
+      {
+        name: 'exclude-assets',
+        resolveId(filePath) {
+          const fileExt = path.extname(filePath);
+          // if fileExt is blank then it should be a "folder" import that wants an index.{js,jsx,ts,tsx} file
+          if (
+            fileExt &&
+            ['.png', '.jpg', '.jpeg', '.gif', '.svg'].indexOf(fileExt) !== -1
+          ) {
+            return { id: filePath, external: true };
+          }
+          return null;
+        }
+      },
+      externals(rollupExternalsConfig),
+      typescriptPlugin(rollupTypescriptConfig),
+      nodeResolve({
+        mainFields: ['module', 'jsnext', 'main'],
+        browser: true,
+        // defaults + .jsx
+        extensions: ['.mjs', '.js', '.jsx', '.json', '.node'],
+        preferBuiltins: true
+      }),
+      commonjs({
+        // use a regex to make sure to include eventual hoisted packages
+        esmExternals: true,
+        requireReturnsDefault: 'namespace'
+      }),
+      json(),
+      jsxPlugin()
+    ]
+  };
+  if (argv.verbose) {
+    console.log(
+      chalk.gray(
+        `Build ${path.relative(paths.cwd, packageDirectory)}: rollup config`
+      )
+    );
+    console.log(
+      chalk.gray(
+        util.inspect(
+          {
+            ...rollupConfig,
+            plugins: [rollupExternalsConfig, rollupTypescriptConfig]
+          },
+          false,
+          3
+        )
+      )
+    );
+  }
+  return rollupConfig;
+}
+
+async function writeTypes(
+  libEsmCwd,
+  tsDefinitionFiles,
+  pathMatchers,
+  typeFilePath
+) {
+  const importedFiles = [];
+  const foundIndex = tsDefinitionFiles.indexOf(typeFilePath);
+  if (foundIndex === -1) return importedFiles;
+  tsDefinitionFiles.splice(foundIndex, 1);
+  const fileBuffer = await fs.readFile(typeFilePath, 'utf8');
+  const node = ts.createSourceFile(
+    typeFilePath,
+    fileBuffer.toString(),
+    ts.ScriptTarget.Latest
+  );
+
+  node.statements.forEach((n) => {
+    if (!n) return;
+    if (
+      n.kind !== ts.SyntaxKind.ImportDeclaration &&
+      n.kind !== ts.SyntaxKind.ExportDeclaration
+    )
+      return;
+    if (!n.moduleSpecifier || !n.moduleSpecifier.text) {
+      return;
+    }
+    const importName = n.moduleSpecifier.text;
+    if (importName.match(/^\.\/|^\.\.\//gi)) {
+      const absolutePath = path.join(path.dirname(typeFilePath), importName);
+      if (!importedFiles.includes(absolutePath)) {
+        importedFiles.push(absolutePath);
+      }
+      return;
+    }
+    let relativeImportPath = importName;
+    // eslint-disable-next-line no-restricted-syntax
+    for (const matcher of pathMatchers) {
+      const match = matcher(importName);
+      if (match) {
+        const absolutePath = path.join(libEsmCwd, match);
+        relativeImportPath = path.relative(
+          path.dirname(typeFilePath),
+          absolutePath
+        );
+        if (relativeImportPath[0] !== '.')
+          relativeImportPath = `./${relativeImportPath}`;
+        if (!importedFiles.includes(absolutePath)) {
+          importedFiles.push(absolutePath);
+        }
+        break;
+      }
+    }
+    n.moduleSpecifier.text = relativeImportPath;
+  });
+  await fs.writeFile(typeFilePath, tsPrinter.printFile(node));
+  return importedFiles;
+}
+
 async function buildPackage(libConfigPath, rootConfigPath) {
   const packageDirectory = path.dirname(libConfigPath);
   const rootPackageDirectory = path.dirname(rootConfigPath);
@@ -97,91 +256,20 @@ async function buildPackage(libConfigPath, rootConfigPath) {
   );
   const libEs5Cwd = path.join(packageDirectory, 'lib/es5');
   try {
-    const rollupExternalsConfig = {
-      packagePath: [
-        packagePath,
-        path.join(rootPackageDirectory, 'package.json')
-      ],
-      deps: true
-    };
-    const rollupTypescriptConfig = {
-      tsconfig: path.join(packageDirectory, 'tsconfig.json')
-    };
     const rollupWarnings = [];
-    const rollupConfig = {
-      input: getBundleInput(packageDirectory),
-      preserveModules: true,
-      treeshake: false,
-      onwarn: (warning) => {
-        // https://github.com/rollup/rollup/blob/0fa9758cb7b1976537ae0875d085669e3a21e918/src/utils/error.ts#L324
-        if (warning.code === 'UNRESOLVED_IMPORT') {
-          rollupWarnings.push(
-            `Failed to resolve the module ${warning.source} imported by ${warning.importer}` +
-              `\nIs the module installed? Note:` +
-              `\n ↳ to inline a module into your bundle, install it to "devDependencies".` +
-              `\n ↳ to depend on a module via import/require, install it to "dependencies".`
-          );
-          return;
-        }
+    const bundleEntries = getBundleInput(packageDirectory);
 
-        if (warning.code === 'UNUSED_EXTERNAL_IMPORT') {
-          console.warn(chalk.yellow('\n', warning.message));
-        }
-        rollupWarnings.push(warning);
-      },
-      plugins: [
-        {
-          name: 'exclude-assets',
-          resolveId(filePath) {
-            const fileExt = path.extname(filePath);
-            // if fileExt is blank then it should be a "folder" import that wants an index.{js,jsx,ts,tsx} file
-            if (
-              fileExt &&
-              ['.png', '.jpg', '.jpeg', '.gif', '.svg'].indexOf(fileExt) !== -1
-            ) {
-              return { id: filePath, external: true };
-            }
-            return null;
-          }
-        },
-        externals(rollupExternalsConfig),
-        typescript(rollupTypescriptConfig),
-        nodeResolve({
-          mainFields: ['module', 'jsnext', 'main'],
-          browser: true,
-          // defaults + .jsx
-          extensions: ['.mjs', '.js', '.jsx', '.json', '.node'],
-          preferBuiltins: true
-        }),
-        commonjs({
-          // use a regex to make sure to include eventual hoisted packages
-          esmExternals: true,
-          requireReturnsDefault: 'namespace'
-        }),
-        json(),
-        jsxPlugin()
-      ]
-    };
-    if (argv.verbose) {
-      console.log(
-        chalk.gray(
-          `Build ${path.relative(paths.cwd, packageDirectory)}: rollup config`
-        )
-      );
-      console.log(
-        chalk.gray(
-          util.inspect(
-            {
-              ...rollupConfig,
-              plugins: [rollupExternalsConfig, rollupTypescriptConfig]
-            },
-            false,
-            3
-          )
-        )
-      );
-    }
-    const bundle = await rollup.rollup(rollupConfig);
+    logTaskStart('Building esm bundle');
+
+    const bundle = await rollup.rollup(
+      loadRollupConfig(
+        packagePath,
+        rootPackageDirectory,
+        packageDirectory,
+        bundleEntries,
+        rollupWarnings
+      )
+    );
     if (argv.verbose) {
       console.log(
         chalk.gray(
@@ -210,7 +298,21 @@ async function buildPackage(libConfigPath, rootConfigPath) {
       );
     }
 
-    await bundle.write({
+    logTaskEnd(true);
+    logTaskStart('Building es5 bundle');
+
+    await configureLibTypescript(true, false, undefined, 'es5');
+    const bundleEs5 = await rollup.rollup(
+      loadRollupConfig(
+        packagePath,
+        rootPackageDirectory,
+        packageDirectory,
+        bundleEntries,
+        rollupWarnings
+      )
+    );
+
+    await bundleEs5.write({
       dir: libEs5Cwd,
       format: 'esm',
       globals: {
@@ -226,6 +328,73 @@ async function buildPackage(libConfigPath, rootConfigPath) {
         )
       );
     }
+
+    logTaskEnd(true);
+    logTaskStart('Compiling d.ts types');
+
+    const { paths: tsPaths, baseUrl } = packageTsconfig.compilerOptions;
+    const pathKeys = Object.keys(tsPaths || {});
+    if (baseUrl) {
+      fs.readdirSync(path.join(packageDirectory, baseUrl), {
+        withFileTypes: true
+      }).forEach((dirent) => {
+        if (!dirent.isDirectory()) return;
+        pathKeys.push(`${dirent.name}/*`);
+      });
+    }
+    const pathMatchers = pathKeys.map((key) => (relativePath) => {
+      if (
+        !relativePath.match(
+          new RegExp(`^${key.replace(/[*/]*$/gi, '.*')}`, 'ig')
+        )
+      )
+        return false;
+      return `./${relativePath}`;
+    });
+    const tsDefinitionFiles = glob.sync(path.join(libEsmCwd, '**/*.d.ts'));
+
+    let transformFiles = bundleEntries.map((bundleEntry) =>
+      path.join(
+        libEsmCwd,
+        path
+          .relative(path.join(packageDirectory, baseUrl), bundleEntry)
+          .replace(/\.tsx?$/, '.d.ts')
+      )
+    );
+    while (transformFiles.length) {
+      // eslint-disable-next-line no-await-in-loop
+      const imports = await Promise.all(
+        transformFiles.map((filepath) =>
+          writeTypes(libEsmCwd, tsDefinitionFiles, pathMatchers, filepath)
+        )
+      );
+      transformFiles = imports
+        .reduce((r, importsArray) => r.concat(importsArray), [])
+        .map((filepath) => {
+          const withExtension = `${filepath}.d.ts`;
+          const withIndex = `${filepath}/index.d.ts`;
+          if (fs.existsSync(withExtension)) return withExtension;
+          if (fs.existsSync(withIndex)) return withIndex;
+          if (fs.existsSync(filepath)) return filepath;
+          throw new Error(
+            `Cannot find file for ${filepath}, check that it is not using any private variables like CONFIG`
+          );
+        })
+        .reduce((r, filepath) => {
+          if (r.includes(filepath)) return r;
+          return r.concat(filepath);
+        }, []);
+    }
+
+    await Promise.all(
+      tsDefinitionFiles
+        .concat(glob.sync(path.join(libEs5Cwd, '**/*.d.ts')))
+        .map((filepath) => fs.rm(filepath))
+    );
+
+    logTaskEnd(true);
+
+    logTaskStart('Copying static assets');
 
     const { assets } = require(path.join(packageDirectory, 'package.json'));
     if (assets) {
@@ -249,6 +418,8 @@ async function buildPackage(libConfigPath, rootConfigPath) {
         );
       }
     }
+
+    logTaskEnd(true);
 
     if (argv.verbose) {
       console.log(
