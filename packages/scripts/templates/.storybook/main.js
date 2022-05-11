c1b7e59b23dda4ce8d1542e4e193570202915e61
SimeonC <simeon@thehavens.co.nz>
2022-05-11 19:13:30 +0900

    fix(scripts): fix compatibilty issues with latest storybook
    Fixing babel issues with latest storybook, if there you had a `.storybook/.babelrc` file to fix babel issues it can be deleted as it should no longer be necessary.

--- packages/scripts/templates/.storybook/main.js
+++ packages/scripts/templates/.storybook/main.js
@@ -1,1 +1,9 @@
-module.exports = require('@tablecheck/scripts/config/storybook.main');
+const mainConfig = require('@tablecheck/scripts/config/storybook.main');
+
+module.exports = {
+  ...mainConfig,
+  features: {
+    babelModeV7: true
+  },
+  addons: [...mainConfig.addons]
+};
