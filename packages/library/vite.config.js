48908eb3b20f3d41ee52fd69763b7a8228b5f9a4
SimeonC <1085899+SimeonC@users.noreply.github.com>
2023-06-30 18:38:50 +0900

    fix: tests and github_actions

--- packages/library/vite.config.js
+++ packages/library/vite.config.js
@@ -2,11 +2,17 @@
 import { defineConfig } from 'vite';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 
+// This is an ultra weird hack to get vite-tsconfig-paths to work.
+// The lib doesn't seem to correctly define esm/cjs (or we have something weird in our setup)
+const resolvedTsPaths =
+  typeof viteTsConfigPaths === 'function'
+    ? viteTsConfigPaths
+    : viteTsConfigPaths.default;
 export default defineConfig({
   cacheDir: '../../node_modules/.vite/library',
 
   plugins: [
-    viteTsConfigPaths({
+    resolvedTsPaths({
       root: '../../',
     }),
   ],
