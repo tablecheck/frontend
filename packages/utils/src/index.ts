d027697dceb810bea9b114bf5af515a7ccb7ad5f
SimeonC <1085899+SimeonC@users.noreply.github.com>
2023-06-30 16:01:03 +0900

    fix: pre-commit scripts and apply formatting

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,9 +1,9 @@
 // Auto Generated file from <root>/scripts/deriveExportsFromSiblings.mjs - Do Not Edit
-export * from './argv.js'
-export * from './config.js'
-export * from './execa.js'
-export * from './lerna.js'
-export * from './packageJson.js'
-export * from './prettier.js'
-export * from './unicodeEmoji.js'
-export * from './userConfig.js'
+export * from './argv.js';
+export * from './config.js';
+export * from './execa.js';
+export * from './lerna.js';
+export * from './packageJson.js';
+export * from './prettier.js';
+export * from './unicodeEmoji.js';
+export * from './userConfig.js';
