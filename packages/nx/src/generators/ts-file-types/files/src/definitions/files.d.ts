4575a559a564f5664afa27bc755868da47bbb084
SimeonC <1085899+SimeonC@users.noreply.github.com>
2023-07-25 10:43:14 +0900

    fix: quality generator and migration include file-types

---
+++ packages/nx/src/generators/ts-file-types/files/src/definitions/files.d.ts
@@ -0,0 +1,16 @@
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+declare module '*.gif' {
+  const content: string;
+  export default content;
+}
+declare module '*.webp' {
+  const content: string;
+  export default content;
+}
---
+++ packages/nx/src/generators/ts-file-types/generator.ts
@@ -0,0 +1,20 @@
+import * as path from 'path';
+
+import { formatFiles, generateFiles, Tree } from '@nx/devkit';
+
+import { FileTypesGeneratorSchema } from './schema';
+
+export async function tsFileTypesGenerator(
+  tree: Tree,
+  schema: FileTypesGeneratorSchema,
+) {
+  generateFiles(
+    tree,
+    path.join(__dirname, 'files', schema.svgAsComponent ? 'srcWithSvg' : 'src'),
+    path.join(tree.root, 'src'),
+    {},
+  );
+  await formatFiles(tree);
+}
+
+export default tsFileTypesGenerator;
