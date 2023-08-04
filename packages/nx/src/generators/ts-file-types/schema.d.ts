bf599766f7742a682f1feedd8895763b4d8524d1
SimeonC <1085899+SimeonC@users.noreply.github.com>
2023-08-04 15:48:05 +0900

    feat: remove library build (use nx rollup) fixes for monorepo

--- packages/nx/src/generators/ts-file-types/schema.d.ts
+++ packages/nx/src/generators/ts-file-types/schema.d.ts
@@ -1,3 +1,4 @@
 export interface FileTypesGeneratorSchema {
   svgAsComponent?: boolean;
+  project: string;
 }
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -2,6 +2,7 @@
 export * from './argv.js';
 export * from './config.js';
 export * from './lerna.js';
+export * from './nx.js';
 export * from './packageJson.js';
 export * from './prettier.js';
 export * from './unicodeEmoji.js';
