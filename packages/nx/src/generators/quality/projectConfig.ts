253bb035111fe5031b621c7cf651ca99ffb68a15
SimeonC <1085899+SimeonC@users.noreply.github.com>
2023-10-02 15:28:34 +0900

    fix: eslint needs extra configs to resolve paths correctly
    Merged the release CI fixes from another branch here as that one is failing for unknow reasons
    
    Fix over-agressive shortest imports

---
+++ packages/nx/src/generators/quality/projectConfig.ts
@@ -0,0 +1,61 @@
+import * as path from 'path';
+
+import {
+  Tree,
+  addProjectConfiguration,
+  readProjectConfiguration,
+  updateProjectConfiguration,
+} from '@nx/devkit';
+import { getNxProjectRoot } from '@tablecheck/frontend-utils';
+import merge from 'lodash/merge';
+
+export function updateProjectConfig(tree: Tree, projectName: string) {
+  const { projectSourceRoot, projectRoot } = getNxProjectRoot(
+    tree,
+    projectName,
+  );
+  const lintTarget = {
+    executor: '@tablecheck/nx:quality',
+    outputs: ['{options.outputFile}'],
+    options: {
+      lintFilePatterns: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'].map(
+        (pattern) =>
+          path.join(
+            '{projectRoot}',
+            path.relative(projectRoot, path.join(projectSourceRoot, pattern)),
+          ),
+      ),
+    },
+  };
+  const lintFormatTarget = merge({}, lintTarget, {
+    options: {
+      fix: true,
+    },
+  });
+  try {
+    const projectConfig = readProjectConfiguration(tree, projectName);
+    updateProjectConfiguration(
+      tree,
+      projectName,
+      merge(projectConfig, {
+        targets: {
+          quality: lintTarget,
+          'quality:format': lintFormatTarget,
+        },
+      }),
+    );
+  } catch (e) {
+    console.error(
+      'Failed to detect existing project config, generating new project.json to run executors',
+      e,
+    );
+    addProjectConfiguration(tree, projectName, {
+      root: '.',
+      sourceRoot: 'src',
+      targets: {
+        quality: lintTarget,
+        'quality:format': lintFormatTarget,
+      },
+    });
+  }
+}
