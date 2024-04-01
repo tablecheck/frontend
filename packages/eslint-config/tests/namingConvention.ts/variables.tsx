791ee3caecda90df0f4b76199ea674838deff8e8
SimeonC <1085899+SimeonC@users.noreply.github.com>
2024-04-01 14:37:24 +0900

    feat: allow `_destroy` as a property
    The `_destroy: true` property is used as a special key in monolith/ruby-on-rails APIs to indicate that the sub-record should be destroyed so we should allow this specific exception.

--- packages/eslint-config/tests/namingConvention.ts/variables.tsx
+++ packages/eslint-config/tests/namingConvention.ts/variables.tsx
@@ -11,3 +11,12 @@ function SomeComponent({ icon: Icon, command }) {
   const { other: Other } = command;
   const iconComponentRef = null;
 }
+
+/** _destroy: true special property */
+saveValue({ id: 'id', _destroy: true });
+cy.wait('@monolithSettings_PUT_/sections/1')
+  .its('request.body')
+  .should('deep.equal', {
+    name: 'section 1',
+    _destroy_background_image: true,
+  });
